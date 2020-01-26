f11b7537dcf35af8043e2da22393bbffe3a7a8f9
sagar <sagarrajak858@gmail.com>
2020-01-26 19:21:10 +0530

    added few dataset

--- seeders/generator.js
+++ seeders/generator.js
@@ -1,11 +1,21 @@
 /* eslint-disable camelcase */
-/* eslint-disable prefer-template */
 /* eslint-disable no-plusplus */
+
+/**
+ * A file will generate random dataset according to product.json
+ * with all possible product and varient
+ * it is assumed all the product have unique name
+ */
 const fs = require('fs');
 const path = require('path');
 const products = require('./products.json');
 
-const ans = [];
+/**
+ * Asuming unique product name
+ */
+let ans = [];
+const objectToWrite = {};
+
 function dfs(product, i, setVal = {}, attr, company_name) {
   if (i < product.length - 1) {
     for (let j = 0; j < product[i].length; j++) {
@@ -20,7 +30,7 @@ function dfs(product, i, setVal = {}, attr, company_name) {
         [attr[i]]: product[i][j],
       };
       const description = {
-        variant_name: Object.values(setVal).join(' ') + ' ' + company_name,
+        variant_name: `${Object.values(setVal).join(' ')} ${company_name}`,
         variant_description: Object.values(setVal).join(' '),
         price: (Math.floor(Math.random() * 100000)),
       };
@@ -32,6 +42,8 @@ function dfs(product, i, setVal = {}, attr, company_name) {
   }
 }
 products.forEach((element) => {
+  ans = [];
   dfs(Object.values(element.values), 0, {}, Object.keys(element.values), `${element.product_name} ${element.company_name}`);
+  objectToWrite[element.product_name] = ans;
 });
-fs.writeFileSync(path.join(__dirname, 'varient.json'), JSON.stringify(ans));
+fs.writeFileSync(path.join(__dirname, 'varient.json'), JSON.stringify(objectToWrite));
